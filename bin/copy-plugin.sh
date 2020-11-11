1c39591aeffb065e745b06ccbe93358465397453
Terry Lin <terry@fontrip.com>
2020-11-11 16:00:58 +0800

    Update shell.

--- bin/copy-plugin.sh
+++ bin/copy-plugin.sh
@@ -3,10 +3,10 @@
 target_path="/tmp/wordpress/wp-content/plugins/cache-master"
 rm -rf ${target_path}
 mkdir ${target_path}
-cp -a ./../inc ${target_path}
-cp -a ./../languages ${target_path}
-cp -a ./../vendor ${target_path}
-cp ./../LICENSE ${target_path}
-cp ./../README.txt ${target_path}
-cp ./../cache-master.php ${target_path}
-cp ./../uninstall.php ${target_path}
+cp -a ./inc ${target_path}
+cp -a ./languages ${target_path}
+cp -a ./vendor ${target_path}
+cp ./LICENSE ${target_path}
+cp ./README.txt ${target_path}
+cp ./cache-master.php ${target_path}
+cp ./uninstall.php ${target_path}
