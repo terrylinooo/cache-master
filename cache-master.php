e567294e3eafa38188225dd883f9eba92c5e88c2
yannicklin <yannicklin.web@gmail.com>
2022-05-31 16:39:07 +1000

    put support for custom_post_type

--- cache-master.php
+++ cache-master.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Plugin Name: Cache Master
  * Plugin URI:  https://github.com/terrylinooo/cache-master
  * Description: A wordpress cache plugin.
- * Version:     2.1.1
- * Author:      Terry Lin
+ * Version:     2.1.2
+ * Author:      Terry Lin, Yannick Lin
  * Author URI:  https://terryl.in/
  * License:     GPL 3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
@@ -12,7 +13,7 @@
  * Domain Path: /languages
  */
 
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
@@ -21,8 +22,8 @@ if ( ! defined( 'WPINC' ) ) {
  * Cache Master plugin uses "SCM" as the prefix on its constants.
  */
 
-if ( ! defined( 'SCM_INC' ) ) {
-	define( 'SCM_INC', true );
+if (!defined('SCM_INC')) {
+	define('SCM_INC', true);
 }
 
 /**
@@ -35,35 +36,35 @@ if ( ! defined( 'SCM_INC' ) ) {
  * SCM_PLUGIN_LANGUAGE_PACK : Translation Language pack.
  * SCM_PLUGIN_VERSION       : SCM plugin version number
  * SCM_PLUGIN_TEXT_DOMAIN   : SCM plugin text domain
- * 
+ *
  * Expected values:
- * 
+ *
  * SCM_PLUGIN_DIR           : {absolute_path}/wp-content/plugins/cache-master/
  * SCM_PLUGIN_URL           : {protocal}://{domain_name}/wp-content/plugins/cache-master/
  * SCM_PLUGIN_PATH          : {absolute_path}/wp-content/plugins/cache-master/cache-master.php
  * SCM_PLUGIN_LANGUAGE_PACK : cache-master/languages
  */
 
-define( 'SCM_PLUGIN_NAME', plugin_basename( __FILE__ ) );
-define( 'SCM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'SCM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'SCM_PLUGIN_PATH', __FILE__ );
-define( 'SCM_PLUGIN_LANGUAGE_PACK', dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-define( 'SCM_PLUGIN_VERSION', '2.1.1' );
-define( 'SCM_CORE_VERSION', '1.3.1' );
-define( 'SCM_PLUGIN_TEXT_DOMAIN', 'cache-master' );
+define('SCM_PLUGIN_NAME', plugin_basename(__FILE__));
+define('SCM_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('SCM_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('SCM_PLUGIN_PATH', __FILE__);
+define('SCM_PLUGIN_LANGUAGE_PACK', dirname(plugin_basename(__FILE__)) . '/languages');
+define('SCM_PLUGIN_VERSION', '2.1.1');
+define('SCM_CORE_VERSION', '1.3.1');
+define('SCM_PLUGIN_TEXT_DOMAIN', 'cache-master');
 
 /**
  * Start to run SCM plugin cores.
  */
 
 // The minimum supported version of PHP.
-if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
+if (version_compare(phpversion(), '7.1.0', '>=')) {
 
 	// This section is for unit testing.
-	if ( defined( 'SCM_PLUGIN_UNIT_TEST' ) ) {
+	if (defined('SCM_PLUGIN_UNIT_TEST')) {
 		$required_files = array(
-			'register', 
+			'register',
 			'setting',
 			'menu',
 			'update-setting',
@@ -79,32 +80,31 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 		require_once SCM_PLUGIN_DIR . 'vendor/autoload.php';
 		require_once SCM_PLUGIN_DIR . 'inc/autoload.php';
 
-		foreach ( $required_files as $file ) {
+		foreach ($required_files as $file) {
 			require_once SCM_PLUGIN_DIR . 'inc/admin/' . $file . '.php';
 		}
 
-		register_activation_hook( __FILE__, 'scm_activation' );
-		register_deactivation_hook( __FILE__, 'scm_deactivation' );
+		register_activation_hook(__FILE__, 'scm_activation');
+		register_deactivation_hook(__FILE__, 'scm_deactivation');
 
 		scm_load_textdomain();
 
 		$cm = new Cache_Master();
 		$cm->init();
-
 	} else {
 
 		// No need to load Cache Master's files when AJAX calls.
-		if ( ! wp_doing_ajax() ) {
+		if (!wp_doing_ajax()) {
 
 			require_once SCM_PLUGIN_DIR . 'inc/helpers.php';
 			require_once SCM_PLUGIN_DIR . 'vendor/autoload.php';
 
-			register_activation_hook( __FILE__, 'scm_activation' );
-			register_deactivation_hook( __FILE__, 'scm_deactivation' );
+			register_activation_hook(__FILE__, 'scm_activation');
+			register_deactivation_hook(__FILE__, 'scm_deactivation');
 
 			scm_load_textdomain();
 
-			if ( is_admin() || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+			if (is_admin() || (defined('WP_CLI') && WP_CLI)) {
 
 				$required_files = array(
 					'register',       // Event: activate and uninstall plugin.
@@ -116,14 +116,13 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 					'admin-bar',      // Add a "Clear Cache" button in admin bar.
 				);
 
-				if ( 'yes' === get_option( 'scm_option_woocommerce_status' ) ) {
+				if ('yes' === get_option('scm_option_woocommerce_status')) {
 					$required_files[] = 'update-woocommerce';
 				}
 
-				foreach ( $required_files as $file ) {
+				foreach ($required_files as $file) {
 					require_once SCM_PLUGIN_DIR . 'inc/admin/' . $file . '.php';
 				}
-
 			} else {
 
 				require_once SCM_PLUGIN_DIR . 'inc/autoload.php';
@@ -131,7 +130,7 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 				$cm->init();
 			}
 		} else {
-			if ( is_admin() ) {
+			if (is_admin()) {
 				require_once SCM_PLUGIN_DIR . 'inc/admin/ajax-action.php';
 			}
 		}
@@ -141,6 +140,6 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 /**
  * Cache Master is open sourced at:
  * https://github.com/terrylinooo/cache-master
- * 
+ *
  * If you have found any bug or have any suggestion, please let me know.
  */
