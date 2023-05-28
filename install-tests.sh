b1f95f768e29fcb9e1bb12818db0f8b4a7d3b7f7
Terry Lin <terrylin.developer@gmail.com>
2023-05-29 02:49:34 +0800

    Update PHP SDK, test with WordPress 6.2.2 and PHP 8.2.5. Add Japanese tranlsation.

--- install-tests.sh
+++ install-tests.sh
@@ -5,3 +5,5 @@
 
 bash bin/install-wp-tests.sh wordpress_unittest shieldon taiwan localhost latest true
 ln -s $(pwd) /tmp/wordpress/wp-content/plugins/cache-master
+composer require --dev phpunit/phpunit
+composer require --dev yoast/phpunit-polyfills
